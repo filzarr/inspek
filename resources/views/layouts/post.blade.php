b01acd2647ab5d2be2932d94824ff2e17d6e32a4
filzarr <filzaramadhan2003@students.ac.id>
2022-08-21 00:10:24 +0700

    baru

---
+++ resources/views/layouts/post.blade.php
@@ -0,0 +1,16 @@
+<div class="col-8 mt-5 ms-lg-5" id="posting" style="background-color: white">
+    <div class="row">
+        <div class="fs-1 ms-lg-5 mt-4 me-3 fw-bolder text-sm-start text-wrap text-break">
+            <p style="'Trebuchet MS', sans-serif;" id="teks">terbaru judul berita inspektorat provinsi sumatera utara </p>
+        </div>
+        <div class="col-12 gap-lg-5 ms-lg-5 fw-lighter"  id="iconi">
+            <i class="bi bi-clock pe-lg-3">  20, januari 2022</i>
+            <i class="bi bi-person pe-lg-3"> admin</i>
+            <i class="bi bi-eye pe-lg-3">  0 View</i>
+            <i class="bi bi-chat-dots pe-lg-3">  3 comment</i>
+        </div>
+        <div class="col mt-lg-5 ms-lg-5 me-lg-5 fs-5" id="artikels">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, distinctio mollitia qui cumque laudantium natus aliquam quis. A, magni architecto? Quisquam eos odit, dolores architecto harum excepturi nam fuga ea!</p>
+        </div>
+    </div>
+</div>
